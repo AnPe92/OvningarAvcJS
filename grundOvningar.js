ff7e3444af818ba0b1a7edd558eea55d2ea97812
AnPe92 <93088524+AnPe92@users.noreply.github.com>
2022-09-12 09:13:00 +0200

    Övningar

--- grundOvningar.js
+++ grundOvningar.js
@@ -156,12 +156,155 @@ function intrestCalc() {
 
 function circleCalc() {
     while (run) {
-        let input = prompt("Enter start capital, number of years and intrest rate ")
+        let input = prompt("Enter if you want circle RADIUS, AREA, PYTT, OR TRIAREA ")
         input === "kill" ? run = false : run = true;
+        let firstPrompt = input.split(" ")[0]
+
+        switch (firstPrompt) {
+            case "radius":
+                input = parseFloat(prompt("Enter radius of circle: "))
+                let serComf = 2 * input * Math.PI
+                console.log("The circles serfumfrens is: ", serComf)
+                break;
+            case "area":
+                input = parseFloat(prompt("Enter radius of circle: "))
+                let cirArea = Math.PI * Math.pow(input, 2)
+                console.log("The circels area is: ", cirArea)
+                break;
+            case "pytt":
+                input = parseFloat(prompt("Enter first side of trinagle: "))
+                let inputTwo = parseFloat(prompt("Enter second side of trinagle: "))
+                let triSide = Math.sqrt(Math.pow(input, 2) + Math.pow(inputTwo, 2))
+                console.log("The third side of the triangel is: ", triSide.toFixed(2))
+                break;
+            case "triarea":
+                input = parseFloat(prompt("Enter first side of trinagle: "))
+                let sideTwo = parseFloat(prompt("Enter second side of trinagle: "))
+                let triSideTwo = Math.sqrt(Math.pow(input, 2) + Math.pow(sideTwo, 2))
+                let area = 1 / 4 * Math.sqrt((input + sideTwo + triSideTwo) * (-input + sideTwo + triSideTwo) *
+                    (input - sideTwo + triSideTwo) * (input + sideTwo - triSideTwo))
+                console.log("The area is: ", area)
+                break;
+        }
+
+    }
+}
+
+// 9.
+
+function shoppingCalc() {
+    while (run) {
+        let input = prompt("Enter number of shopping trips:  ")
+        input === "kill" ? run = false : run = true;
+        let numOfTrips = Number(input);
+        let sum = 0;
+        for (let i = 0; i < numOfTrips; i++) {
+            input = prompt("Enter money spent on shopping trip: ", i + 1)
+            input === "kill" ? run = false : run = true;
+            let tripsum = Number(input)
+            sum = sum + tripsum;
+        }
+        console.log("Total amount spent is: ", sum)
+    }
+}
 
 
+//10.
+
+function getMedValue() {
+    while (run) {
+        let input = prompt("Enter amount of numbers to calc: ")
+        input === "kill" ? run = false : run = true;
+        let numOfnums = Number(input);
+        let sumOfNums = 0;
+        for (let i = 0; i < numOfnums; i++) {
+            input = prompt("Enter number: ", i + 1)
+            input === "kill" ? run = false : run = true;
+            let numEntered = Number(input);
+            sumOfNums = sumOfNums + numEntered;
+        }
+        let res = sumOfNums / numOfnums;
+        console.log("Average value of entered numbers are: ", res)
+    }
+}
+
+//11.
+function checkDiv() {
+    while (run) {
+        let input = prompt("Enter number to check: ")
+        input === "kill" ? run = false : run = true;
+        let num = Number(input)
+        if (num % 2 === 0 || num % 3 === 0 || num % 5 === 0) {
+            console.log("its divicable by 2, 3 or 5")
+        } else {
+            console.log("Not divicable!")
+        }
+    }
+
+}
+
+//12.
+
+function runToExit() {
+    while (run) {
+        let input = prompt("Enter text, exit to quit: ")
+        input === "exit" ? run = false : run = true;
     }
 }
 
 
 
+//13.
+
+const MORSE_CODE = {
+    "-----": "0",
+    ".----": "1",
+    "..---": "2",
+    "...--": "3",
+    "....-": "4",
+    ".....": "5",
+    "-....": "6",
+    "--...": "7",
+    "---..": "8",
+    "----.": "9",
+    ".-": "A",
+    "-...": "B",
+    "-.-.": "C",
+    "-..": "D",
+    ".": "E",
+    "..-.": "F",
+    "--.": "G",
+    "....": "H",
+    "..": "I",
+    ".---": "J",
+    "-.-": "K",
+    ".-..": "L",
+    "--": "M",
+    "-.": "N",
+    "---": "O",
+    ".--.": "P",
+    "--.-": "Q",
+    ".-.": "R",
+    "...": "S",
+    "-": "T",
+    "..-": "U",
+    "...-": "V",
+    ".--": "W",
+    "-..-": "X",
+    "-.--": "Y",
+    "--..": "Z",
+    "-.-.--": "!",
+    ".-.-.-": ".",
+    "--..--": ","
+};
+
+function engMorse() {
+    while (run) {
+        let input = prompt("Enter text or morsecode to reverse: ")
+        input === "exit" ? run = false : run = true;
+        let words = input.split(" ");
+        for (let word in words) {
+
+        }
+    }
+}
